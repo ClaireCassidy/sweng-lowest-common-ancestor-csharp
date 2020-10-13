3b9f339efbd224bf716ea50dbf7105a7e0843e66
ClaireCassidy <clairecassidy0@hotmail.com>
2020-10-13 19:22:55 +0100

    initial commit for c# impl

---
+++ lca-csharp/lca-csharp/BinaryTree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lca_csharp
+{
+    class BinaryTree
+    {
+        public void sayHello()
+        {
+            Console.WriteLine("Hi there");
+        }
+    }
+}
---
+++ lca-csharp/lca-csharp/LowestCommonAncestor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lca_csharp
+{
+    class LowestCommonAncestor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!!!!!!!!!!");
+
+            BinaryTree myTree = new BinaryTree();
+            myTree.sayHello();
+        }
+
+    }
+}
